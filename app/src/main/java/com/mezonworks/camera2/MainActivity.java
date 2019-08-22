552aff56ba22ba641d152f9d8835277e9c19effc
Azek <sudoazek@gmail.com>
2019-08-22 19:30:49 +0900

    Internal content uri

--- app/src/main/java/com/mezonworks/camera2/MainActivity.java
+++ app/src/main/java/com/mezonworks/camera2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.mezonworks.camera2;
 
+import android.content.Intent;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        onClick();
+    }
+
+    public void onClick(View v) {
+        Intent myIntent = new Intent();
+        myIntent.setAction(Intent.ACTION_VIEW);
+        myIntent.setData(MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivity(myIntent);
     }
 }
